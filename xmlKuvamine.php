48472fbe5233040bfc56bb4e84bfeb455cb1604b
George Teemus <george.teemus@gmail.com>
2026-01-13 14:00:24 +0200

    ERR

--- xmlKuvamine.php
+++ xmlKuvamine.php
@@ -1,5 +1,6 @@
 <?php
 $opilased=simplexml_load_file("opilased.xml");
+$feed=simplexml_load_file("https://www.err.ee/feed");
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,5 +13,39 @@ $opilased=simplexml_load_file("opilased.xml");
 //1. õpilase nimi
 echo "1.õpilase nimi".$opilased->opilane[0]->nimi;
 ?>
+<table>
+    <tr>
+        <th>Õpilase nimi</th>
+        <th>Isikukood</th>
+        <th>Eriala</th>
+        <th>Elukoht</th>
+    </tr>
+    <?php
+    foreach($opilased->opilase as $opilane){
+        echo "<tr>";
+        echo "<td>".$opilane->nimi."</td>";
+        echo "<td>".$opilane->isikukood."</td>";
+        echo "<td>".$opilane->eriala."</td>";
+        echo "<td>".$opilane->elukoht->linn."
+,".$opilane->elukoht->maakond."</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+<h1>RSS uudiste lugemine</h1>
+<?php
+echo "<ul>";
+foreach($feed->channel->item as $item){
+    echo "<li>";
+    echo "<a href=' $item->link' target='_blank'".$item->title."</a>";
+    echo $item->desctiption;
+    echo " Kuupäev:".$item->title;
+    echo "</li>";
+
+}
+
+echo "</ul>"
+?>
+
 </body>
 </html>
